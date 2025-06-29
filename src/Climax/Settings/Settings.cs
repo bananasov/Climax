3dbb4988af8dd72f58cb4dad84d3753f1f213fdf
bananasov <56086819+bananasov@users.noreply.github.com>
2025-06-29 14:20:51 +0200

    feat: Add basic configuration for Climax

---
+++ src/Climax/Settings/Settings.cs
@@ -0,0 +1,10 @@
+﻿using BepInEx.Configuration;
+
+namespace Climax.Config;
+
+public class ClimaxSettings(ConfigFile config)
+{
+    public ConfigEntry<string> ServerURL = config.Bind("Buttplug", "Server URL", "ws://127.0.0.1:12345", "The URL where the Intiface server is hosted");
+
+    public ConfigEntry<bool> SprintingVibrationEnabled = config.Bind("Vibrations.Sprinting", "Enabled", true, "Whether or not using sprinting makes your toys vibrate");
+}
