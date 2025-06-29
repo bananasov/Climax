25fc72049803ab76dda862e8c1d7b3b2479f3844
bananasov <56086819+bananasov@users.noreply.github.com>
2025-06-29 14:21:07 +0200

    chore: Extract hooks to specific files

---
+++ src/Climax/Hooks/CharacterDetour.cs
@@ -0,0 +1,32 @@
+﻿using MonoDetour.HookGen;
+
+namespace Climax.Hooks;
+
+[MonoDetourTargets(typeof(Character))]
+public static class CharacterDetour
+{
+    private static bool _wasSprinting = false;
+    private static bool _isCurrentlySprinting = false;
+    
+    public static void Init()
+    {
+        On.Character.Update.Prefix(Prefix_Update);
+    }
+    
+    private static void Prefix_Update(Character self)
+    {
+        _isCurrentlySprinting = self.data.isSprinting;
+
+        switch (_wasSprinting)
+        {
+            case false when _isCurrentlySprinting:
+                _wasSprinting = true;
+                Plugin.DeviceManager.VibrateConnectedDevices(0.1f);
+                break;
+            case true when !_isCurrentlySprinting:
+                _wasSprinting = false;
+                Plugin.DeviceManager.StopConnectedDevices();
+                break;
+        }
+    }
+}
